c442d59059c0dd6a008fbc8a7c33a9b6ac380867
davie <mukungidavid@gmail.com>
2020-08-17 15:17:51 +0300

    added dashboard feedback

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -24,5 +24,42 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["Posture"]
             }
         }
+    },
+    'Source of referral' : function (formName, formFieldValues) {
+        var sourceofreferral = formFieldValues['Source of referral'];
+        if (sourceofreferral === "Other source of referral (Specify)") {
+            return {
+                show: ["Specify other source of referral"]
+            }
+        } else {
+            return {
+                hide: ["Specify other source of referral"]
+            }
+        }
+    },
+    'Presenting complaints' : function (formName, formFieldValues) {
+        var presentingcomplaints = formFieldValues['Presenting complaints'];
+        if (presentingcomplaints === "Feeling Low") {
+            return {
+                show: ["Other presenting complaints"]
+            }
+        } else {
+            return {
+                hide: ["Other presenting complaints"]
+            }
+        }
+    },
+    'Trigger factors/history of presenting complaints' : function (formName, formFieldValues) {
+        var historypresentingcomplaints = formFieldValues['Trigger factors/history of presenting complaints'];
+        if (historypresentingcomplaints === "Other trigger factors (Please specify)") {
+            return {
+                enable: ["Specify other trigger factors"] 
+            }
+        } else {
+            return {
+                disable: ["Specify other trigger factors"]
+            }
+        }
     }
+
 };
